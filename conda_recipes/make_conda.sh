3fda94deac38b5aeaa5d1a87809935463fdbaf03
John ZuHone <jzuhone@gmail.com>
2018-08-27 16:13:02 -0400

    Updates

--- conda_recipes/make_conda.sh
+++ conda_recipes/make_conda.sh
@@ -6,6 +6,6 @@ VERSION="$3"
 
 export MACOSX_DEPLOYMENT_TARGET="10.9"
 
-conda build ${SOFT}-stable --python 2.7 --numpy 1.11 -c astropy
-conda build ${SOFT}-stable --python 3.5 --numpy 1.11 -c astropy
-conda build ${SOFT}-stable --python 3.6 --numpy 1.11 -c astropy
+conda build ${SOFT}-stable --python 2.7 --numpy 1.13 -c astropy
+conda build ${SOFT}-stable --python 3.5 --numpy 1.13 -c astropy
+conda build ${SOFT}-stable --python 3.6 --numpy 1.13 -c astropy
