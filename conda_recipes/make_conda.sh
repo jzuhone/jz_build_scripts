f040709f5b1c939328c385c9ca45072a8ad59018
John ZuHone <jzuhone@gmail.com>
2021-01-20 10:27:03 -0500

    pyxsim 2.3.0

--- conda_recipes/make_conda.sh
+++ conda_recipes/make_conda.sh
@@ -1,11 +1,12 @@
 #!/bin/zsh
 
-ACCOUNT="lynx-x-ray-observatory"
-SOFT="soxs"
+ACCOUNT="jzuhone"
+SOFT="pyxsim"
 VERSION="2.3.0"
 
 export MACOSX_DEPLOYMENT_TARGET="10.9"
 
-conda build ${SOFT}-stable --python 3.6 --numpy 1.15 -c astropy
-conda build ${SOFT}-stable --python 3.7 --numpy 1.16 -c astropy
-conda build ${SOFT}-stable --python 3.8 --numpy 1.18 -c astropy
+conda build ${SOFT}-stable --python 3.6 --numpy 1.15 -c astropy -c jzuhone --no-anaconda-upload
+conda build ${SOFT}-stable --python 3.7 --numpy 1.16 -c astropy -c jzuhone --no-anaconda-upload
+conda build ${SOFT}-stable --python 3.8 --numpy 1.18 -c astropy -c jzuhone --no-anaconda-upload
+#conda build ${SOFT}-stable --python 3.9 --numpy 1.19 -c astropy
