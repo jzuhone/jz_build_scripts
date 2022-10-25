a57e98ecb39054f5ddac0dd1898769e521db1421
John ZuHone <jzuhone@gmail.com>
2022-10-25 12:50:19 -0400

    Updates

--- conda_recipes/make_conda.sh
+++ conda_recipes/make_conda.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
 ACCOUNT="jzuhone"
-SOFT="soxs"
+SOFT="pyxsim"
 
 export MACOSX_DEPLOYMENT_TARGET="10.9"
 
-conda build ${SOFT} --python 3.8 --numpy 1.19 -c astropy -c jzuhone --no-anaconda-upload
-conda build ${SOFT} --python 3.9 --numpy 1.20 -c astropy -c jzuhone --no-anaconda-upload
-conda build ${SOFT} --python 3.10 --numpy 1.21 -c astropy -c jzuhone --no-anaconda-upload
+conda build ${SOFT} --python 3.8 --numpy 1.19 -c astropy -c jzuhone
+conda build ${SOFT} --python 3.9 --numpy 1.21 -c astropy -c jzuhone
+conda build ${SOFT} --python 3.10 --numpy 1.21 -c astropy -c jzuhone
