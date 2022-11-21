26b605d8ca3aabe087e1197afe546178dbc5778f
John ZuHone <jzuhone@gmail.com>
2022-11-20 22:54:07 -0500

    Update pyxsim

--- conda_recipes/make_conda.sh
+++ conda_recipes/make_conda.sh
@@ -8,3 +8,4 @@ export MACOSX_DEPLOYMENT_TARGET="10.9"
 conda build ${SOFT} --python 3.8 --numpy 1.19 -c astropy -c jzuhone
 conda build ${SOFT} --python 3.9 --numpy 1.21 -c astropy -c jzuhone
 conda build ${SOFT} --python 3.10 --numpy 1.21 -c astropy -c jzuhone
+#conda build ${SOFT} --python 3.11 --numpy 1.22 -c astropy -c jzuhone
